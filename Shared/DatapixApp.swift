773e135e4a442d22f0ca85160ad30f83b08472ef
Tadreik Campbell <tadreik@gmail.com>
2021-01-30 10:39:18 -0500

    Initial Commit

---
+++ Shared/DatapixApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatapixApp.swift
+//  Shared
+//
+//  Created by Tadreik Campbell on 1/30/21.
+//
+
+import SwiftUI
+
+@main
+struct DatapixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
